961d1429bd178757765feb0df2e9af2a7ac53160
WORK <WORK@WORKPC>
2018-12-05 02:48:12 +0200

    Initial commit

---
+++ BloodRage/BloodRage.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using PoeHUD.Plugins;
+using PoeHUD.Poe.Components;
+using static BloodRage.WinApiMouse;
+
+namespace BloodRage
+{
+    public class BloodRage : BaseSettingsPlugin<BloodRageSettings>
+    {
+        public override void Render()
+        {
+            if (Settings.Enable)
+            {
+                var buffs = GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Life>().Buffs;
+                if (!buffs.Exists(b => b.Name == "blood_rage")) //blood_rage
+                    {
+                        Keyboard.KeyPress(Settings.pressedKey.Value);
+                        //lasttime = DateTime.Now;
+                    }
+            }
+        }
+    }
+
+    public class Keyboard
+    {
+        [DllImport("user32.dll")]
+        private static extern uint keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        private const int KEYEVENTF_EXTENDEDKEY = 0x0001;
+        private const int KEYEVENTF_KEYUP = 0x0002;
+
+        private const int ACTION_DELAY = 70;
+
+
+        [DllImport("user32.dll")]
+        public static extern bool BlockInput(bool fBlockIt);
+
+        public static void KeyDown(Keys key)
+        {
+            keybd_event((byte)key, 0, KEYEVENTF_EXTENDEDKEY | 0, 0);
+        }
+
+        public static void KeyUp(Keys key)
+        {
+            keybd_event((byte)key, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0); //0x7F
+        }
+
+        public static void KeyPress(Keys key)
+        {
+            KeyDown(key);
+            Thread.Sleep(ACTION_DELAY);
+            KeyUp(key);
+        }
+
+        [DllImport("USER32.dll")]
+        private static extern short GetKeyState(int nVirtKey);
+
+        public static bool IsKeyDown(int nVirtKey)
+        {
+            return GetKeyState(nVirtKey) < 0;
+        }
+    }
+
+    public static class WinApiMouse
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(out Point lpMousePoint);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        #region Structs/Enums
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            Move = 0x00000001,
+            Absolute = 0x00008000,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+        }
+
+        #endregion
+    }
+}
+
+
+
+
+
---
+++ BloodRage/BloodRageSettings.cs
@@ -0,0 +1,20 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using System.Windows.Forms;
+namespace BloodRage
+{
+    public class BloodRageSettings : SettingsBase
+    {
+        public BloodRageSettings()
+        {
+            //delay = new RangeNode<int>(4, 1, 10);
+            pressedKey = Keys.E;
+        }
+       
+        [Menu("Key", 1)]
+        public HotkeyNode pressedKey { get; set; }
+
+        //[Menu("Delay After Pressing Key", 2)]
+        //public RangeNode<int> delay { get; set; }
+    }
+}
